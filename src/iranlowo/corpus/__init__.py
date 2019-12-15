7189dd826daa840769ad84ed12a5da463a882bbf
Olamyy <olamyy53@gmail.com>
2019-12-15 18:00:41 +0100

    Moving corpus module

--- src/iranlowo/corpus/__init__.py
+++ src/iranlowo/corpus/__init__.py
@@ -1,2 +1,1 @@
 from .corpus import Corpus, DirectoryCorpus
-from .loaders import YorubaCorpusLoader
--- tests/test_loaders.py
+++ tests/test_loaders.py
@@ -5,8 +5,16 @@ from iranlowo import corpus
 
 class TestCoprusLoader(unittest.TestCase):
     def setUp(self):
+        self.yoruba_blog_path = ""
         self.owe_loader = corpus.OweLoader
 
     def test_load_owe(self):
         with self.assertRaises(NotADirectoryError):
             self.owe_loader()
+
+    def test_load_yorubablog(self):
+        yor = corpus.YorubaBlogCorpus(path=self.yoruba_blog_path)
+        self.assertIsInstance(corpus.Corpus, yor)
+
+    def load_yoruba_text_coprus(self):
+        pass
